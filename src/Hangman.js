4ec7ea9ef9902acb9c57fd5e8b9d498b3f25a1d3
Daylan <daylanberry@gmail.com>
2020-04-29 17:24:55 -0700

    hangman game

---
+++ src/Hangman.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import { randomWord } from './words'
+import "./Hangman.css";
+import img0 from "./0.jpg";
+import img1 from "./1.jpg";
+import img2 from "./2.jpg";
+import img3 from "./3.jpg";
+import img4 from "./4.jpg";
+import img5 from "./5.jpg";
+import img6 from "./6.jpg";
+
+class Hangman extends Component {
+  /** by default, allow 6 guesses and use provided gallows images. */
+  static defaultProps = {
+    maxWrong: 6,
+    images: [img0, img1, img2, img3, img4, img5, img6],
+    word: randomWord()
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { nWrong: 0, guessed: new Set(), answer: this.props.word };
+    this.handleGuess = this.handleGuess.bind(this);
+    this.restartGame = this.restartGame.bind(this)
+  }
+
+  /** guessedWord: show current-state of word:
+    if guessed letters are {a,p,e}, show "app_e" for "apple"
+  */
+  restartGame(){
+    this.setState({
+      nWrong: 0,
+      guessed: new Set(),
+      answer: randomWord()
+    })
+  }
+
+  guessedWord() {
+    return this.state.answer
+      .split("")
+      .map(ltr => (this.state.guessed.has(ltr) ? ltr : "_"));
+  }
+
+  /** handleGuest: handle a guessed letter:
+    - add to guessed letters
+    - if not in answer, increase number-wrong guesses
+  */
+  handleGuess(evt) {
+    let ltr = evt.target.value;
+    this.setState(st => ({
+      guessed: st.guessed.add(ltr),
+      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1)
+    }));
+  }
+
+  /** generateButtons: return array of letter buttons to render */
+  generateButtons() {
+    const buttons = "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
+      <button
+        key={ltr}
+        value={ltr}
+        onClick={this.handleGuess}
+        disabled={this.state.guessed.has(ltr)}
+      >
+        {ltr}
+      </button>
+    ));
+
+    return this.state.nWrong < this.props.maxWrong ? buttons : (
+      <div>
+        <span>You lose: </span>
+        <span>The correct word was: {this.state.answer}</span>
+        <button style={{width: '200px'}} onClick={() => this.restartGame()}>Click to restart!</button>
+      </div>
+    )
+  }
+
+  /** render: render game */
+  render() {
+    return (
+      <div className='Hangman'>
+        <h1>Hangman</h1>
+        <img src={this.props.images[this.state.nWrong]} />
+        <p>Number of wrong guesses: {this.state.nWrong}</p>
+        <p className='Hangman-word'>{this.guessedWord()}</p>
+        <div className='Hangman-btns'>{this.generateButtons()}</div>
+
+      </div>
+    );
+  }
+}
+
+export default Hangman;
